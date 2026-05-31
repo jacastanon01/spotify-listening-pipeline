dd974b88a6d476b3667675a7091a74e57032b507
Jacob Castanon <jacastanon01@gmail.com>
2026-05-31 11:07:15 -0500

    cursor functionality and main logic

--- fetch_recent.py
+++ fetch_recent.py
@@ -6,17 +6,26 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from datetime import datetime, timezone
 
+from db import create_connection, insert_stream, insert_track
 from models import Stream, Track
+from pipeline import is_track
 
 dotenv.load_dotenv()
 
 
 def iso_to_unix_ms(ts: str) -> int:
+    """
+    :param ts: ISO timestamp to convert
+    :return: unix timestamp in milliseconds
+    """
     dt = datetime.fromisoformat(ts.replace("Z", "+00:00"))
     return int(dt.timestamp() * 1000)
 
 
 def init_spotipy() -> spotipy.Spotify:
+    """
+    Uses spotipy to initilize and return authorized spotify client to use
+    """
     sp = spotipy.Spotify(
         auth_manager=SpotifyOAuth(
             client_id=os.getenv("SPOTIPY_CLIENT_ID"),
@@ -29,16 +38,20 @@ def init_spotipy() -> spotipy.Spotify:
     return sp
 
 
-def fetch_recent(sp: spotipy.Spotify, cursor: int) -> dict[str, Any] | None:
+def fetch_recent(
+    sp: spotipy.Spotify, after: int | None = None
+) -> dict[str, Any] | None:
     """
+    uses spotipy web api to get 50 most recently played tracks
     :param sp: Authenticated spotipy client
-    :return: JSON Response from Spotify API
+    :return: JSON Response from Spotify API endpoint
     """
-    return sp.current_user_recently_played(after=cursor)
+    return sp.current_user_recently_played(after=after)
 
 
 def transform(item: dict[str, Any]) -> tuple[Track, Stream]:
     """
+    Extracts data from repsonse, validates it and returns instantiated classes that organize into a format the DB will accept
     :param item: item from spotify most recently played API reponse
     :return: A tuple containing a Track dataclass instance and a Stream dataclass instance.
     """
@@ -67,7 +80,15 @@ def transform(item: dict[str, Any]) -> tuple[Track, Stream]:
     track = Track(
         uri=track_uri,
         name=track_name,
-        artist=track_artists_list[0].get("name", "Unknown Artist"),
+        artist=(
+            track_artists_list[0].get(
+                "name", "Unknown Artist"
+            )  # get name field from first artist listed in list
+            if isinstance(
+                track_artists_list[0], dict
+            )  # verifies that first object is dictionary that contains name field
+            else "Unknown Artist"
+        ),
         album=album_name,
     )
     stream = Stream(
@@ -82,14 +103,42 @@ def transform(item: dict[str, Any]) -> tuple[Track, Stream]:
     return track, stream
 
 
-"""
-def save_cursor(ts):
-"""
+def save_cursor(ts: str, cursor_cache: str) -> None:
+    """
+    :param ts: ISO timestamp of listening event to log
+    :param cursor_cache: location of file that has cursor log
+    """
+    with open(cursor_cache, "w") as f:
+        f.write(ts)
 
-"""
-def load_cursor():
-"""
 
-"""
-def main():
-"""
+def load_cursor(cursor_cache) -> str | None:
+    """
+    :param cursor_cache: location of file that has cursor log to read from
+    :return: cursor saved from local file
+    """
+    try:
+        with open(cursor_cache, "r") as f:
+            cursor = f.read()
+        return cursor
+    except FileNotFoundError as e:
+        print(f"Error loading from {cursor_cache}: {e}")
+        return None
+
+
+def main() -> None:
+    sp = init_spotipy()
+    cursor = load_cursor(".spotify_cursor")
+    results = fetch_recent(sp, after=iso_to_unix_ms(cursor) if cursor else None)
+    if results:
+        items = results["items"]
+        conn = create_connection("listening_history.db")
+        for item in items:
+            if is_track(item):
+                track, stream = transform(item)
+                insert_track(conn, track)
+                insert_stream(conn, stream)
+        conn.commit()
+        if items:
+            save_cursor(items[0]["played_at"], ".spotify_cursor")
+        conn.close()
