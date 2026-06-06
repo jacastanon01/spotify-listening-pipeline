256aec2e21171db41f151d6a436d7b34687884b2
Jacob Castanon <jacastanon01@gmail.com>
2026-06-06 17:28:36 -0500

    Finished overview tab, oncluding charts for artists and tracks analyzed by minutes played using plotly for a better visualization

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
+import plotly.express as px
 
 from db import create_connection
 from queries import GET_YEARLY_PLAYS_MINUTES, GET_YEARLY_TOP_ARTISTS, GET_YEARLY_TOP_TRACKS
@@ -46,8 +47,15 @@ with overview:
     time_df = get_minutes_by_year(conn, start_year, end_year)
     artist_df = get_top_artists_by_year(conn, start_year, end_year)
     tracks_df = get_top_tracks_by_year(conn, start_year, end_year)
+    tracks_df["label"] = tracks_df["name"] + " - " + tracks_df["artist"]
 
     if time_df.empty:
         st.warning(f"No listening history found between {start_year} and {end_year}")
     else:
         st.line_chart(time_df, x="year", y="minutes_played")
+
+        artists_fig = px.bar(artist_df, x="minutes_played", y="artist", orientation="h")
+        st.plotly_chart(artists_fig)
+
+        tracks_fig = px.bar(tracks_df, x="minutes_played", y="label", orientation="h")
+        st.plotly_chart(tracks_fig)
