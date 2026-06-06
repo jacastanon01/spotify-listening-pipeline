2f829988752e55016fd2f9c1307c76f22f35fa0e
Jacob Castanon <jacastanon01@gmail.com>
2026-06-06 16:43:26 -0500

    Set up streamlit overview line graph to display minutes listening on Spotify. Adjusted time range using a slider. Made sure data was rendered in UI

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
+from tracemalloc import start
+
 import streamlit as st
 import sqlite3
 import pandas as pd
 
 from db import create_connection
-from queries import GET_YEARLY_PLAYS_MINUTES, GET_YEARLY_TOP_ARTISTS
+from queries import GET_YEARLY_PLAYS_MINUTES, GET_YEARLY_TOP_ARTISTS, GET_YEARLY_TOP_TRACKS
 
 # ============================================================
 # SETUP
 # ============================================================
 @st.cache_resource
 def get_connection() -> sqlite3.Connection | None:
-    conn = create_connection("listening_history.db")
+    conn = create_connection("listening_history.db", check_same_thread=False)
     return conn
 
 st.title("Listening History Project")
@@ -21,18 +23,32 @@ conn = get_connection()
 # DATA FUNCTIONS
 # ============================================================
 @st.cache_data
-def get_plays_by_year(_conn: sqlite3.Connection | None, start_year: int = 2013, end_year: int = 2026) -> pd.DataFrame:
+def get_minutes_by_year(_conn: sqlite3.Connection | None, start_year: int = 2013, end_year: int = 2026) -> pd.DataFrame:
     return pd.read_sql_query(GET_YEARLY_PLAYS_MINUTES, _conn, params=[start_year, end_year])
 
 @st.cache_data
-def get_top_artists(_conn: sqlite3.Connection | None, start_year: int = 2013, end_year: int = 2026) -> pd.DataFrame:
+def get_top_artists_by_year(_conn: sqlite3.Connection | None, start_year: int = 2013, end_year: int = 2026) -> pd.DataFrame:
     return pd.read_sql_query(GET_YEARLY_TOP_ARTISTS, _conn, params=[start_year, end_year])
+
+@st.cache_data
+def get_top_tracks_by_year(_conn: sqlite3.Connection | None, start_year: int = 2013, end_year: int = 2026) -> pd.DataFrame:
+    return pd.read_sql_query(GET_YEARLY_TOP_TRACKS, _conn, params=[start_year, end_year])
 
 # ============================================================
 # APP LAYOUT
 # ============================================================
 
+overview, habits, phases = st.tabs(["Overview", "Habits", "Phases"])
+
 # OVERVIEW
-overview_sections = ["time", "artists", "tracks"]
-plays_by_year_data = get_plays_by_year(conn)
-st.write(plays_by_year_data)
+st.header("Music through the years")
+start_year, end_year = st.slider("Time Range(Yr)", 2013, 2026, (2013, 2026))
+
+time_df = get_minutes_by_year(conn, start_year, end_year)
+artist_df = get_top_artists_by_year(conn, start_year, end_year)
+tracks_df = get_top_tracks_by_year(conn, start_year, end_year)
+
+if time_df.empty:
+    st.warning(f"No listening history found between {start_year} and {end_year}")
+else:
+    st.line_chart(time_df, x="year", y="minutes_played")
