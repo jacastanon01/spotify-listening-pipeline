7645bfa4ddbc7c10151bd308440209a6fcd0f2cb
Jacob Castanon <jacastanon01@gmail.com>
2026-06-06 14:54:40 -0500

    Rough implementation of overview tab. Will need ot add dynamic slider to modify time range by year and flesh out functions for artists and tracks

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 
 from db import create_connection
-from queries import GET_YEARLY_PLAYS_MINUTES
+from queries import GET_YEARLY_PLAYS_MINUTES, GET_YEARLY_TOP_ARTISTS
 
 # ============================================================
 # SETUP
@@ -21,12 +21,18 @@ conn = get_connection()
 # DATA FUNCTIONS
 # ============================================================
 @st.cache_data
-def get_plays_by_year(_conn: sqlite3.Connection | None) -> pd.DataFrame:
-    return pd.read_sql_query(GET_YEARLY_PLAYS_MINUTES, _conn)
+def get_plays_by_year(_conn: sqlite3.Connection | None, start_year: int = 2013, end_year: int = 2026) -> pd.DataFrame:
+    return pd.read_sql_query(GET_YEARLY_PLAYS_MINUTES, _conn, params=[start_year, end_year])
 
+@st.cache_data
+def get_top_artists(_conn: sqlite3.Connection | None, start_year: int = 2013, end_year: int = 2026) -> pd.DataFrame:
+    return pd.read_sql_query(GET_YEARLY_TOP_ARTISTS, _conn, params=[start_year, end_year])
 
 # ============================================================
-# LAYOUT
+# APP LAYOUT
 # ============================================================
+
+# OVERVIEW
+overview_sections = ["time", "artists", "tracks"]
 plays_by_year_data = get_plays_by_year(conn)
 st.write(plays_by_year_data)
