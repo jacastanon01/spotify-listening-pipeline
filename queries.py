5be2e44740e2550d6717245af5287d275bfd0161
Jacob Castanon <jacastanon01@gmail.com>
2026-06-28 16:51:01 -0500

    local script to query for titles that may need rapidfuzz ratio applied to

--- queries.py
+++ queries.py
@@ -7,3 +7,13 @@ GET_RAW_STREAMS_REASONS = """
 SELECT reason_start, reason_end 
 FROM streams;
 """
+
+# how many itunes artists appear anywhere in Spotify tracks?
+GET_FUZZY_ARTISTS_MATCH = """
+SELECT COUNT(DISTINCT it.artist_normalized)
+FROM itunes_tracks it
+WHERE it.artist_normalized IN (
+    SELECT DISTINCT lower(t.artist) FROM tracks t
+);
+"""
+
